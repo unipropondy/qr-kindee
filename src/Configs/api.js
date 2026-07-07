038c18633563577c41ae0509dbf98dae26651f84
bavani18 <bavaniabavani18@gmail.com>
2026-07-07 12:07:44 +0530

    kindee

---
+++ src/Configs/api.js
@@ -0,0 +1,9 @@
+export const BASE_URL = "https://qr-kindee-production.up.railway.app"; //std
+
+
+
+// export const BASE_URL = "http://localhost:5000";
+
+// const BASE_URL = "https://smabackoffice-production-fc6a.up.railway.app";
+
+// export default BASE_URL;
