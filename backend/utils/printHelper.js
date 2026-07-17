cef7c2ffbdab472682b574e202cb16e455c6b980
lokesh9976 <lokeshkrishnamoorthy2002@gmail.com>
2026-07-17 14:23:17 +0530

    Change KOT/KDS modifiers style to tall size (remove bold)

--- backend/utils/printHelper.js
+++ backend/utils/printHelper.js
@@ -183,7 +183,7 @@ function formatKOTThermalText(data, itemsForPrinter, type) {
     const modifiers = item.modifiers || (item.ModifiersJSON ? JSON.parse(item.ModifiersJSON) : []);
     if (modifiers && modifiers.length > 0) {
       modifiers.forEach((m) => {
-        t += `[L]    <B>+ ${m.ModifierName || m.name}</B>\n`;
+        t += `[L]<font size='tall'>    + ${m.ModifierName || m.name}</font>\n`;
       });
     }
 
