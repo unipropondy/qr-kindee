74396a84e5fa457497a50470ab0d650adcec210b
lokesh9976 <lokeshkrishnamoorthy2002@gmail.com>
2026-07-17 14:12:10 +0530

    Use bold styling for KOT/KDS modifiers to make them mid-size

--- backend/utils/printHelper.js
+++ backend/utils/printHelper.js
@@ -183,7 +183,7 @@ function formatKOTThermalText(data, itemsForPrinter, type) {
     const modifiers = item.modifiers || (item.ModifiersJSON ? JSON.parse(item.ModifiersJSON) : []);
     if (modifiers && modifiers.length > 0) {
       modifiers.forEach((m) => {
-        t += `[L]<font size='big'>    + ${m.ModifierName || m.name}</font>\n`;
+        t += `[L]    <B>+ ${m.ModifierName || m.name}</B>\n`;
       });
     }
 
